fba1435f4f86e796ecf8c1549dffcf8ba67cbf9a
AVVS <v@makeomatic.ru>
2018-04-24 13:27:39 -0700

    fix: scripts

--- scripts/add-client.sh
+++ scripts/add-client.sh
@@ -5,5 +5,5 @@ set -e
 : "${USERNAME:?Need to set USERNAME as non-empty, for example: user\@example.com}"
 
 # with a passphrase (recommended)
-docker-compose run --rm openvpn easyrsa build-client-full $USERNAME
-docker-compose run --rm openvpn ovpn_getclient $USERNAME > ./data/clients/$USERNAME.ovpn
+docker exec openvpn easyrsa build-client-full $USERNAME
+docker exec openvpn ovpn_getclient $USERNAME > ./data/clients/$USERNAME.ovpn
--- scripts/remove-client.sh
+++ scripts/remove-client.sh
@@ -4,4 +4,4 @@ set -e
 
 : "${USERNAME:?Need to set USERNAME as non-empty, for example: user\@example.com}"
 
-docker-compose run --rm openvpn ovpn_revokeclient $USERNAME
+docker exec openvpn ovpn_revokeclient $USERNAME
