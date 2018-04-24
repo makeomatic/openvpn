38cc79ec91e3f636ebcba6c7b2d78025d2c8b01c
AVVS <v@makeomatic.ru>
2018-04-24 13:28:58 -0700

    fix: use interactive mode

--- scripts/add-client.sh
+++ scripts/add-client.sh
@@ -5,5 +5,5 @@ set -e
 : "${USERNAME:?Need to set USERNAME as non-empty, for example: user\@example.com}"
 
 # with a passphrase (recommended)
-docker exec ovpn easyrsa build-client-full $USERNAME
+docker exec -it ovpn easyrsa build-client-full $USERNAME
 docker exec ovpn ovpn_getclient $USERNAME > ./data/clients/$USERNAME.ovpn
